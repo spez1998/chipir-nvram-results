782b4a98ee86d8c1367820edfd595f975282daea
spez1998 <samman30@live.com>
2021-07-16 10:14:29 +0100

    Removed comment

--- new_analyser.py
+++ new_analyser.py
@@ -23,7 +23,6 @@ class Analyser:
         self.data = pd.read_csv(self.log_names[0], error_bad_lines=False)
         for i in range(1, len(self.log_names)):
             print("\n")
-            #print(self.log_names[i])
             self.data_next = pd.read_csv(self.log_names[i], error_bad_lines=False)
             self.data_frames = [self.data, self.data_next]
             self.data = pd.concat(self.data_frames, ignore_index=True)
